84e8befc41c1cc93fbcc92a7c37f2aa7ba35e4b2
Justin Paterson <JustinPaterson@rocketmortgage.com>
2022-12-29 16:57:03 -0500

    Improve code coverage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -14,10 +17,37 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('navbarOpen should be set to true', () => {
+    component.navbarOpen = true;
+    expect(component.navbarOpen).toBeTruthy();
+  });
+
+  it('closeMenu() should set navbarOpen to false', () => {
+    component.navbarOpen = true;
+    component.closeMenu();
+    expect(component.navbarOpen).toBeFalsy();
+  });
+
+  it('toggleNavbar() should set navbarOpen to true, when navbarOpen is false', () => {
+    component.navbarOpen = false;
+    component.toggleNavbar();
+    expect(component.navbarOpen).toBeTruthy();
+  });
+
+  it('toggleNavbar() should set navbarOpen to false, when navbarOpen is true', () => {
+    component.navbarOpen = true;
+    component.toggleNavbar();
+    expect(component.navbarOpen).toBeFalsy();
   });
 
 });
